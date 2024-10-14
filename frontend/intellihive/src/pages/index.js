e33fcbcbcb1f1206c41a9c722fea693476366127
Negin <113136968+neginheidarii@users.noreply.github.com>
2024-10-14 19:12:36 -0400

    imported all screens in index

--- frontend/intellihive/src/pages/index.js
+++ frontend/intellihive/src/pages/index.js
@@ -1,7 +1,11 @@
 import Dashboard from "./Dashboard";
 import Login from "./Login";
-import Register from "./Register";
+import SignUp from "./SignUp";
 import Transactions from "./Transactions";
+import PayBills from "./PayBills";
+import Profile from "./Profile";
+import Settings from "./Settings";
+import TransferMoney from "./TransferMoney";
 
 
-export { Dashboard, Login, Register, Transactions };
+export { Dashboard, Login, SignUp, Transactions, PayBills, Profile, Settings, TransferMoney };

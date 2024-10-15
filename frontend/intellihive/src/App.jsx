cdfd789041e2b732f297eac056547ecd4e1b23dd
Negin <113136968+neginheidarii@users.noreply.github.com>
2024-10-14 21:24:46 -0400

    Implemented navigation for navbar using react router

--- frontend/intellihive/src/App.jsx
+++ frontend/intellihive/src/App.jsx
@@ -1,19 +1,37 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import  Dashboard  from "./pages/Dashboard";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import {
+  Dashboard,
+  PayBills,
+  Profile,
+  Settings,
+  Transactions,
+  TransferMoney,
+} from "./pages/index";
+import Navbar from "./components/Navbar";
+
 
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
 
   return (
-    <>
-      
-      <Dashboard />
-  
-    </>
+    <Router>
+      <div className="">
+     
+        <div className="">
+          {/* Adjust margin to match navbar width */}
+          <Routes>
+            <Route path="/" element={<Dashboard />} />
+            <Route path="/pay-bills" element={<PayBills />} />
+            <Route path="/profile" element={<Profile />} />
+            <Route path="/settings" element={<Settings />} />
+            <Route path="/transactions" element={<Transactions />} />
+            <Route path="/transfer-money" element={<TransferMoney />} />
+          </Routes>
+        </div>
+      </div>
+    </Router>
   );
 }
 
